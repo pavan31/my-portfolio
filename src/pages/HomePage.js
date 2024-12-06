b12fc2ea033611eb3ae768327b34107f540aeb08
seshu31 <pavan.seshu@matrixintel.com>
2024-12-06 16:04:50 +0530

    changes in router and projects

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,11 +2,13 @@ import React from "react";
 import ContactUsPage from "./ContactUsPage";
 import styles from "../assets/styles/HomePageStyles";
 import AboutMePage from "./AboutMePage";
+import ProjectsComponent from "../components/ProjectsComponent";
 
 const HomePage = () => {
   return (
     <div style={styles.container}>
       <AboutMePage />
+      {/* <ProjectsComponent /> */}
       <ContactUsPage />
     </div>
   );
